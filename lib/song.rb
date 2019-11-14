2ad3d46f61b8b658eede37482d007a25bfee9791
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-14 19:53:33 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,11 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+    # def initialize(name)
+    #   @name = name
+    #   @@all << self
+    # end
+
   def self.all
     @@all
   end
@@ -10,4 +15,62 @@ class Song
     self.class.all << self
   end
 
-end
+  def self.create
+    song = self.new
+    song.save
+    return song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    return song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    return song
+  end
+
+  def self.find_by_name(name)
+    @@all.find { |song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    if !find_by_name(name)
+      create_by_name(name)
+    else
+      return find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|song| song.name }
+  end
+
+  def self.new_from_filename(name)
+    song = self.new
+    artist = name.split(" - ")
+    title = artist[1].split(".")
+    song.artist_name = artist[0]
+    song.name = title[0]
+    return song
+  end
+
+  def self.create_from_filename(name)
+    song = self.new
+    artist = name.split(" - ")
+    title = artist[1].split(".")
+    song.artist_name = artist[0]
+    song.name = title[0]
+    song.save
+    return song
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+end #end of class
