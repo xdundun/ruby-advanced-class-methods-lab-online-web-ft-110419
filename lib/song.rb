d392fdfb20427a2f69f31b8591d9274931bf2c3b
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-14 19:54:22 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,7 @@ class Song
     @@all.sort_by {|song| song.name }
   end
 
-  def self.new_from_filename(name)
+  def self.new_from_filename(name)  # UPDATE WITH REGEX FOR SPLIT
     song = self.new
     artist = name.split(" - ")
     title = artist[1].split(".")
@@ -59,7 +59,7 @@ class Song
     return song
   end
 
-  def self.create_from_filename(name)
+  def self.create_from_filename(name) # UPDATE WITH REGEX FOR SPLIT
     song = self.new
     artist = name.split(" - ")
     title = artist[1].split(".")
